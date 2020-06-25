5b4379f0aa34c59de3f4fe30fe876be8ae72f1e4
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-25 09:04:22 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  resources :users
+  # post 'user_token' => 'user_token#create'
+  # resources :users
+  post '/signin', to: 'user_token#create'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
