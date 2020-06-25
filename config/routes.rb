8b7771084612e1777eb42b97c0f4677cbb8fec30
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-25 09:09:01 +0000

    scope the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # post 'user_token' => 'user_token#create'
   # resources :users
-  post '/signin', to: 'user_token#create'
-  post '/signup', to: 'users#create'
+  scope '/auth' do
+    post '/signin', to: 'user_token#create'
+    post '/signup', to: 'users#create'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
